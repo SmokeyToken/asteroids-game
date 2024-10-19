09ca67df157664ea4195fb8d0bccf0f955359665
SmokeyToken <jeffrey.braam1996@gmail.com>
2024-10-19 18:16:56 -0500

    Fourth Commit: Player is drawn and can move around screen

--- main.py
+++ main.py
@@ -3,28 +3,47 @@
 # throughout this file
 from constants import *
 import pygame
+from player import Player
+from circleshape import CircleShape
+
 def main():
 	pygame.init()
 	clock = pygame.time.Clock()
 	dt = 0
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 	print("Starting asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
+
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+	Player.containers = (updatable, drawable)
+
+	# Need to instantiate the player after putting the class in the container
+	player_instance = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
 	while True:
 		# Check if user has closed window
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
-		# Fill the screen with black
-		screen.fill((0, 0, 0))
-		# Render player on screen constantly
-		player.draw(screen)
-		# Constantly refresh the black
-		pygame.display.flip()
 		# Limit frame rate to 60 FPS
 		dt = clock.tick(60) / 1000.0
+
+		# Fill the screen with black
+		screen.fill((0, 0, 0))	
+		
+		# Update everything that should be updated
+		for thing in updatable:
+			thing.update(dt)
+		# Render everything on screen constantly
+		for thing in drawable:
+			thing.draw(screen)
+
+		# Constantly update the screen
+		pygame.display.flip()
+		
+
 		
 if __name__ == "__main__":
 	main()
